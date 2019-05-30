a378312ddbddd0d6259b23c2fe1739470954d854
Dan-Vy Le <danvyle85@gmail.com>
2019-05-30 02:01:53 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -16,20 +16,7 @@ def reformat_languages(languages)
    new_hash  
 end
 
-# def reformat_languages(languages)
-#   # your code here
-#   new_hash = {}
-#     languages.each do |language, kind|
-#       kind.each do |style_key, style_value|
-#         style_value.each do |k, v|
-#           new_hash[style_key] = style_value
-#           new_hash[:style] = [language]
-#         end
-#       end
-#     end
-  
-#   return new_hash
-# end
+# 
 
 # {
 #   :ruby => {
